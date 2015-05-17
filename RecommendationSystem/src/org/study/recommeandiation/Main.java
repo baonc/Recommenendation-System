83f5a7c446db6f90b300b999bff8ac9f5fe27dde
BaoNC <baonc93@gmail.com>
2015-05-17 14:12:20 +0700

    test user profile

--- RecommendationSystem/src/org/study/recommeandiation/Main.java
+++ RecommendationSystem/src/org/study/recommeandiation/Main.java
@@ -12,11 +12,21 @@ public class Main {
 		/*UtilityMatrix uti = new UtilityMatrix();
 		uti.generatingUtilityMatrix();
 		uti.test();*/
-		String testString = "a		b";
+		/*String testString = "a		b";
 		String testSplit[] = testString.split("\t");
 		
 		for(int i = 0; i < testSplit.length; i++) {
 			System.out.println("\"" + testSplit[i] + "\"");
+		}*/
+		
+		UserProfiles user = new UserProfiles();
+		user.readFile();
+		User userProfile[] = user.getUserProfiles();
+		
+		for(int i = 0; i < userProfile.length; i++) {
+			System.out.println("ID: " + userProfile[0] + ", gender: " + userProfile[1] + 
+					", age: " + userProfile[2] + ", country: " + userProfile[3] + 
+					", signup: " + userProfile[4]);
 		}
 	}
 }
