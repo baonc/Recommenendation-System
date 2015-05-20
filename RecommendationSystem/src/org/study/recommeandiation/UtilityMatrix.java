1264298ef7394017d99773b5ce883fde3f2d03e2
BaoNC <baonc93@gmail.com>
2015-05-20 15:27:39 +0700

    update utility matrix

--- RecommendationSystem/src/org/study/recommeandiation/UtilityMatrix.java
+++ RecommendationSystem/src/org/study/recommeandiation/UtilityMatrix.java
@@ -1,12 +1,19 @@
 package org.study.recommeandiation;
 
+import static java.nio.file.StandardOpenOption.CREATE;
+
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 /**
  * Class create utility matrix from file userid-timestamp-artid-artname-traid-
@@ -19,6 +26,8 @@ import java.util.HashMap;
 public class UtilityMatrix {
 	private HashMap<String, Integer> utilityHashMap;									  // hash map contain Key: userId:artistId and value: 
 																					      // number of trackid on artist id.
+	private ArrayList<Artist> artistList;												  // array list containt track of a artist
+
 	private static final String INPUT_FILE = "data/userid-timestamp-artid-"				  // input file
 			+ "artname-traid-traname.tsv";
 	
@@ -32,10 +41,20 @@ public class UtilityMatrix {
 	}
 
 	/**
+	 * Getter of artist list
+	 * 
+	 * @return	: artist list
+	 */
+	public ArrayList<Artist> getArtistList() {
+		return this.artistList;
+	}
+
+	/**
 	 * Function read input file and put data to utilityHashMap.
 	 */
 	public void generatingUtilityMatrix() {
 		this.utilityHashMap = new HashMap<String, Integer>();
+		this.artistList = new ArrayList<Artist>();
 		
 		try(InputStream in = Files.newInputStream(Paths.get(UtilityMatrix.INPUT_FILE));
 				BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
@@ -68,6 +87,8 @@ public class UtilityMatrix {
 			}
 			
 			System.out.println("DONE.");
+			reader.close();
+			in.close();
 		} catch(IOException ioe) {
 			ioe.printStackTrace();
 		}
@@ -81,5 +102,34 @@ public class UtilityMatrix {
 			System.out.println(key + ": " + this.utilityHashMap.get(key));
 		}
 		System.out.println("Size: " + this.utilityHashMap.size());
+	}
+	
+	/**
+	 * Function write artist - track to file<br>
+	 * File name: artist_track
+	 */
+	public void writeArtistTrackLis() {
+		Path file = Paths.get("data/artist_track");
+		
+		try(OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, 
+				CREATE))) {
+			for(int i = 0; i < this.artistList.size(); i++) {
+				out.write(this.artistList.get(i).getArtistId().getBytes());
+				out.write("\t".getBytes());
+				out.write(this.artistList.get(i).getArtistName().getBytes());
+				out.write("\t".getBytes());
+				Set<String> trackSet = this.artistList.get(i).getTrackList();
+				for(String track : trackSet) {
+					out.write(track.getBytes());
+					out.write(" ".getBytes());
+				}
+				out.write("\n".getBytes());
+				System.out.println("Writing line: " + i);
+			}
+		} catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+		this.artistList = null;
 	}
 }
